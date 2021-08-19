0d9149e3c657dead4024a8ffeddea97cc218a452
Anandhan <anandhanedom@gmail.com>
2021-08-20 02:30:00 +0530

    feat:add base setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import CongressImg from "../../assets/c_logo.png";
+
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="transparent"
+      variant="dark"
+      fixed="top"
+    >
+      <Container fluid>
+        <Navbar.Brand className="d-flex align-items-center">
+          <img
+            alt="congress-logo"
+            src={CongressImg}
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+          />
+          <div className="custom-nav-brand">
+            <div className="custom-nav-brand-heading">Dr. Shashi Tharoor</div>
+            <div className="custom-nav-brand-sub-heading">
+              MP Thiruvananthapuram & Author
+            </div>
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link eventKey={2}>About</Nav.Link>
+            <Nav.Link eventKey={3}>In Parliament</Nav.Link>
+            <Nav.Link eventKey={6}>CTF</Nav.Link>
+            <Button variant="outline-info" className="rounded-0 border-2">
+              Contact Us
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./index.scss";
+
+const Hero = () => {
+  return <div className="hero-section">
+    
+  </div>;
+};
+
+export default Hero;
